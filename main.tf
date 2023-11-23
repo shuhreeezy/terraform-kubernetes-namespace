f1087f9a85aed181fe1cf0f31c7764f5ba9e8253
shuhreeezy <shuhrat.babadjanov.123@gmail.com>
2023-11-22 18:41:28 -0600

    added size

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
 resource "kubernetes_namespace" "example" {
   metadata {
     annotations = var.annotations
-
     labels = var.labels
-
     name = var.name
+  }
+}
+
+
+resource "kubernetes_resource_quota" "pod-limit" {
+  metadata {
+    name = "pod-limit"
+  }
+  spec {
+    hard = {
+      pods = 1000
+    }
+    scopes = ["BestEffort"]
   }
 }
